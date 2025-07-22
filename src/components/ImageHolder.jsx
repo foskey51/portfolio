6c5fbbbb25295b3375392718396d9808afb795b4
foskey51 <dhanushhs51@gmail.com>
2025-07-22 19:40:16 +0530

    fix: image overflow in placeholder

--- src/components/ImageHolder.jsx
+++ src/components/ImageHolder.jsx
@@ -20,14 +20,14 @@ const ImageHolder = () => {
 
     return (
         <div className="flex w-[90%] h-[100%] md:w-full md:h-full border rounded-lg bg-gradient-to-bl backdrop-blur-lg dark:from-white/15 dark:to-black/4 mx-auto shadow-md shadow-gray-400 dark:shadow-md dark:shadow-gray-400 dark:text-white text-black mb-6 md:mb-0">
-            <div className="relative w-full h-full">
+            <div className="realtive w-full h-full">
                 {!isLoaded && (
                     <div className="absolute inset-0 flex items-center justify-center">
                         <a className='font-mono text-black dark:text-white'>Loading...</a>
                     </div>
                 )}
                 <img
-                    className={`h-full w-full object-fill transition-opacity duration-700 ease-in-out ${isLoaded ? 'opacity-100' : 'opacity-0'}`}
+                    className={`object-fill md:w-full md:h-full rounded-lg transition-opacity duration-700 ease-in-out ${isLoaded ? 'opacity-100' : 'opacity-0'}`}
                     src={randomPic}
                     loading="lazy"
                     onLoad={() => setIsLoaded(true)}
